a0b0c58e1e7d8d3bdea19a23340154c3772515d8
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-12-15 02:23:17 +0000

    Fix: Remove unnecessary + symbols from the main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,11 +10,10 @@ export default function Home() {
     <Bounded center={true}>
       <div className="flex flex-col justify-center items-center w-full h-full mt-0 md:mt-20 gap-4">
         <h1 className="flex flex-col items-center">
-          +{' '}
           <span className="text-[40px] md:text-[80px] font-bold text-primary_green">
             {t('title')}
           </span>
-          <span className="text-[40px] md:text-[80px] text-white">{t('subtitle')}</span>+{' '}
+          <span className="text-[40px] md:text-[80px] text-white">{t('subtitle')}</span>
         </h1>
         <p className="text-[16px] md:text-[20px] w-full md:max-w-[60%] text-white text-center">
           {t('description')}
